afef3ed97d0e0ddb3759aed0698d268cd993baa5
Gabriel esteban ruiz meneses <gabriel_ruiz21@hotmail.com>
2021-01-20 17:22:19 -0500

    working project

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://classed:XQ1dcGLItWFFjZLd@cluster0.ovdfk.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
+
+//XQ1dcGLItWFFjZLd
